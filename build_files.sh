e6226298c3db14a0871a1e00cd3dc2d74dd550ec
Samuel Cavadas <scdevbrazil@gmail.com>
2024-05-09 10:14:34 -0300

    comentou make migrations

--- build_files.sh
+++ build_files.sh
@@ -15,9 +15,9 @@ echo "Building the project..."
 python -m pip install -r requirements.txt
 
 # Make migrations
-echo "Making migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+# echo "Making migrations..."
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
