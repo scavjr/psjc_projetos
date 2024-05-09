5783301922602f224b8cd9536b7ba48f8398203f
Samuel Cavadas <scdevbrazil@gmail.com>
2024-05-08 23:55:26 -0300

    alterado

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,24 @@
 # build_files.sh
 
-python3 pip install -r requirements.txt
-python3 manage.py collectstatic --no-input --clear
+# build_files.sh
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.12 -m venv venv
+source venv/bin/activate
+
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
